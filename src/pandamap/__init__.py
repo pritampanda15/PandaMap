8327f879a8aee07cb03908264aa57a091650ae15
Pritam Kumar Panda <pritampkp15@gmail.com>
2025-04-03 00:10:05 -0700

    Update __init__.py

--- src/pandamap/__init__.py
+++ src/pandamap/__init__.py
@@ -5,7 +5,7 @@ A Python package for visualizing protein-ligand interactions
 with 2D ligand structure representation and minimal external dependencies.
 """
 
-__version__ = "3.0"
+__version__ = "2.5"
 
 from .core import SimpleLigandStructure, HybridProtLigMapper
 
